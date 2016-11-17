05f58a6999cc396bed2060a4df00dadcb2130c9c
Priscilla Rodriguez <priss@prisskreative.com>
2016-11-16 23:20:11 -0500

    Create the site controller and route - add the react-rails-gem

--- Gemfile
+++ Gemfile
@@ -34,6 +34,9 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # Making the code DRY-er
 gem 'responders', '~> 2.0'
 
+# React-rails 
+gem 'react-rails'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
 
+  root to: 'site#index'
+
   namespace :api do
     namespace :v1 do 
      resources :items, only: [:index, :create, :destroy, :update] 
     end 
   end
+  
+end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -60,4 +64,4 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+
