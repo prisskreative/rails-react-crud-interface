6367e6529e71bb8e99d7e906319a90b291ccb9ae
Priscilla Rodriguez <priss@prisskreative.com>
2016-11-16 23:11:41 -0500

    Add db seeds and update the gem 'responders', '~> 2.0'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  # change this
+  # protect_from_forgery with: :exception
+  # for
+  protect_from_forgery with: :null_session
 end
